35599da83c49f7db9e34b86694fcf19fa8c11f74
crystalDf <chendong333@gmail.com>
2018-06-27 22:34:54 +0800

    LearningRxJavaFilterOperators

---
+++ app/src/main/java/com/star/learningrxjavafilteroperators/MainActivity.java
@@ -0,0 +1,53 @@
+package com.star.learningrxjavafilteroperators;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "RxJava";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Observable
+                .create((ObservableOnSubscribe<Integer>) emitter -> {
+
+                    emitter.onNext(1);
+                    emitter.onNext(2);
+                    emitter.onNext(3);
+                    emitter.onNext(4);
+                    emitter.onNext(5);
+                })
+                .filter(integer -> integer > 3)
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "开始采用subscribe连接");
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.d(TAG, "对Next事件" + integer + "作出响应"  );
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "对Error事件作出响应");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "对Complete事件作出响应");
+                    }
+                });
+    }
+}
